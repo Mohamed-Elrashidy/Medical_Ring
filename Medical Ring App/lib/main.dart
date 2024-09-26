52c3e46241ba353b1a083a601fa4998b0342e7ce
Mohamed-Elrashidy <meahef@gmail.com>
2024-09-27 01:58:57 +0300

    init testing branch

--- Medical Ring App/lib/main.dart
+++ Medical Ring App/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medical_ring_app/core/theming/theming_bloc/themeing_cubit.dart';
 import 'package:medical_ring_app/medical_ring_app.dart';
-// Mohamed Elrashidy
+// Created By Mohamed Elrashidy
 void main() {
 
   runApp(
